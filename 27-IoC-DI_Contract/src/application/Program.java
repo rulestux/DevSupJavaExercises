a52b6fdf9b19e0113c3e0d6a3571caed46535d72
Tux Rules <rulestux@gmail.com>
2025-10-02 17:12:58 -0300

    Inversion of Control and Dependency Injection: Contract.

---
+++ 27-IoC-DI_Contract/src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Enter contract data:");
+        System.out.print("Number: #");
+        int number = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Date (DD/MM/YYYY): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Contract value: $");
+        double totalValue = sc.nextDouble();
+        sc.nextLine();
+
+        Contract obj = new Contract(number, date, totalValue);
+
+        System.out.print("Enter the number of installments: #");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        ContractService contractService = new ContractService(new PaypalService());
+
+        contractService.processContract(obj, n);
+
+        System.out.println();
+        System.out.println("Installments:");
+        for (Installment installment : obj.getInstallments()) {
+            System.out.println(installment);
+        }
+
+        sc.close();
+    }
+}
